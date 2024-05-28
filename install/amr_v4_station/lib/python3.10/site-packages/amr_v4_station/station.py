93d7be251f978573f16813605a0f615834e33020
colin <colin.smith@fisherco.com>
2024-05-28 11:48:17 -0400

    final product

--- install/amr_v4_station/lib/python3.10/site-packages/amr_v4_station/station.py
+++ install/amr_v4_station/lib/python3.10/site-packages/amr_v4_station/station.py
@@ -4,13 +4,14 @@ import rclpy
 import rclpy.executors
 import sys
 from std_msgs.msg import Bool
+import RPi.GPIO as gpio
 import pyodbc
 
-sensor1Pin = 1
-sensor2Pin = 2
-light1Pin = 3
-light2Pin = 4
-light3Pin = 5
+sensor1Pin = 11
+sensor2Pin = 13
+light1Pin = 16
+light2Pin = 18
+light3Pin = 22
 
 class Station:
     def __init__(self, name):
@@ -18,67 +19,70 @@ class Station:
         self.sensor1 = False
         self.sensor2 = False
         self.light = Light()
-        self.inPlace = True
+        self.inPlace = False
         self.stationClear = True
         self.stationNode = rclpy.create_node('station_node')
         self.stationPublisher = self.stationNode.create_publisher(Bool, 'station209A/stationStatus', 10)
         
         self.cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=fisher-agc.database.windows.net;DATABASE=Fisher_AGC;UID=fisher_agc;PWD=tTp##L86?qM!4iG7')
         self.cursor = self.cnxn.cursor()
+
+        gpio.setwarnings(False)
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(sensor1Pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+        gpio.setup(sensor2Pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+        gpio.setup(light1Pin, gpio.OUT)
+        gpio.setup(light2Pin, gpio.OUT)
+        gpio.setup(light3Pin, gpio.OUT)
+
         
     def fetchStationClearFromDatabase(self):
-        result = self.cursor.execute("SELECT cart_in_place FROM Station WHERE station_name = '" + self.name + "'")
-        self.stationClear = result.fetchone()[0]
-        print(self.stationClear)
+        self.cursor.execute("SELECT cart_in_place FROM Station WHERE station_name = ?", self.name)
+        result = self.cursor.fetchone()
+        if result:
+            self.stationClear = result[0]
 
     
     def checkSensors(self):
-        # self.sensor1 = gpio.input(sensor1Pin)
-        # self.sensor2 = gpio.input(sensor2Pin)
+        self.sensor1 = gpio.input(sensor1Pin)
+        self.sensor2 = gpio.input(sensor2Pin)
+        print("S1: " + str(self.sensor1) + " S2: " + str(self.sensor2))
         self.inPlace = self.sensor1 and self.sensor2
     
     def sendStationStatus(self):
-        if(not self.stationClear):
-            self.cursor.execute("UPDATE Station SET cart_in_place = 0 WHERE station_name = '" + self.name + "'")
-        else:
-            self.cursor.execute("UPDATE Station SET cart_in_place = 1 WHERE station_name = '" + self.name + "'")
-    
+        self.cursor.execute("UPDATE Station SET cart_in_place = ? WHERE station_name = ?", (1 if self.inPlace else 0, self.name))
+        self.cnxn.commit()
     
     def run(self):
         while rclpy.ok():
             self.light.changeColor(False, False)
-            # self.checkSensors()
+            self.checkSensors()
             
             if self.inPlace:
                 self.stationClear = False
                 self.sendStationStatus()
+                print('tote in place')
         
-            while(not self.stationClear):
-                # self.checkSensors()
+            while not self.stationClear:
+                self.checkSensors()
                 self.fetchStationClearFromDatabase()
                 
-                if(self.inPlace):
+                if self.inPlace:
                     self.light.changeColor(True, True)
-                    self.stationClear = True
-                    self.inPlace = False
                 else:
                     self.light.changeColor(True, False)
+                    print('tote moved')
                     
-            self.cursor.execute("UPDATE Station SET cart_in_place = 1 WHERE station_name = '" + self.name + "'")
+            self.sendStationStatus()
             self.fetchStationClearFromDatabase()
+            print('station cleared')
             
-                    
-            
-            
-
-
 class Light:
     def __init__(self):
         self.r = False
         self.g = False
         self.b = False
         
-    
     def changeColor(self, s1, s2):
         if s1 and s2:
             self.r = False
@@ -93,26 +97,23 @@ class Light:
             self.g = True
             self.b = False
         
-        # GPIO.output(light1Pin, self.r)
-        # GPIO.output(light2Pin, self.g)
-        # GPIO.output(light3Pin, self.b)
-        # print("R: ", self.r, " G: ", self.g, " B: ", self.b)
+        gpio.output(light1Pin, self.r)
+        gpio.output(light2Pin, self.g)
+        gpio.output(light3Pin, self.b)
 
-def main(args = None):
-    rclpy.init(args = args)
+def main(args=None):
+    rclpy.init(args=args)
     
-    # Temporary node to fetch the parameter
-    node = rclpy.create_node('temporary_node_for_param')
+    if len(sys.argv) < 2:
+        print("Usage: station.py <station_name>")
+        sys.exit(1)
     
-    # Declare the parameter
-    node.declare_parameter('station_name', 'station_default')
+    station_name = sys.argv[1]
     
-    # Get the parameter
-    station_name = node.get_parameter('station_name').get_parameter_value().string_value
-    
-    # Destroy the temporary node
-    node.destroy_node()
     print('station name: ' + station_name)
     station = Station(station_name)
     station.run()
     rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
